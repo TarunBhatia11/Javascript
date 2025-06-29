071a14e28bd906eb47fcf13a94b0a0c84d9771d5
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-06-30 02:53:42 +0530

    js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+// Data Types
+
+"use strict"; // treat all JS code as newer version
+
+// alert("hello")
+
+
+
+let a = "tarun" //string => ""
+let b = 13 // number
+let c = true // boolean=> true/false
+let d = null // standalone value
+let e; //undefined 
+
+console.log(typeof undefined) // output: undefined
+console.log(typeof null) //output: object
+console.log(typeof a) //output: string
+console.log(typeof b) //output: number
+console.log(typeof c)//output: boolean 
