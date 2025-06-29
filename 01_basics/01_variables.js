63ff1490421ba0537a25e9477c6755c6dbe5b74c
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-06-30 02:23:01 +0530

    js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+// Here we'll discuss about variables and comments
+
+// 1. COMMENTS
+
+
+/* 
+
+1. ( /* ) this help us to comment the functionality in multiple paragraph 
+
+2. ( // ) this help us to comment the functionality in single line 
+
+*/
+
+
+
+// 2. VARIABLES
+
+// What is Variables? Variables is an identity which indicates and contain some values in it. Example: Container
+
+const accountId = 12345 //here accountId denotes variable name which store 12345 
+
+console.log(accountId);
+
+let accountPassword = "TarunBhatia@123"
+
+console.log(accountPassword);
+
+
+console.table([accountId, accountPassword])
