0d01c81e4fee97d877f8e94bc9ad376431fa7a47
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-06-30 19:34:57 +0530

    js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -2,25 +2,84 @@
 
 let score = "null"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 
 let a = undefined
-console.log(Number(a))
+// console.log(Number(a))
 
 
 //for binary in boolean
 let b = 1
-console.log(Boolean(b))
+// console.log(Boolean(b))
 
 // binary to string
 let c = true
-console.log(String(c))
+// console.log(String(c))
+
+// **********Operations***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 3);
+
+
+
+
+let string1 = "hello"
+let string2 = " tarun"
+
+// console.log(string1 + string2);
+
+// console.log("2" + 1);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+"");
+// console.log(+true);
+// console.log(true+); error
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+//prefix or postfix
+let gameCounter = 100
+
+
+
+// console.log(--gameCounter); prefix incre
+// console.log(++gameCounter); prefix decre
+
+// console.log(gameCounter++); postfix incre
+// console.log(gameCounter--); postfix deecre
+
+
+
+
+
+
+
+
+
+
+
+
+
