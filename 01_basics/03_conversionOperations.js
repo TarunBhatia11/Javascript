4131e70adb2dea530f27e4783a04d6c03fd51781
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-06-30 19:18:05 +0530

    js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+//conversions in operations
+
+let score = "null"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+
+let a = undefined
+console.log(Number(a))
+
+
+//for binary in boolean
+let b = 1
+console.log(Boolean(b))
+
+// binary to string
+let c = true
+console.log(String(c))
