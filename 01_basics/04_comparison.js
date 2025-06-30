158b9ded248a00eb32d1b0043abd346912b44d75
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-06-30 19:44:20 +0530

    js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,39 @@
+//comparison
+
+/*
+
+> greator
+< less
+>= greator than
+<= less than
+== equalTo
+!= not equal to
+
+
+*/
+
+// console.log("2" > 1); true
+// console.log("02" > 1); true
+
+// console.log(null > 0);
+// console.log(null == 0);
+
+// console.log(null >= 0);
+
+
+//strict check "==="
+
+// normal check + data type check
+
+// console.log("2" === 3);
+
+// console.log(2 === 2);
+
+
+// console.log(null === 0);
+
+// console.log(undefined === null);
+
+
+
+
