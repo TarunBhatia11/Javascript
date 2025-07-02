9e945934260e9317bf668186041ff603012608a7
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-07-02 20:39:46 +0530

    Array Part

---
+++ 02_basics/11_array2.js
@@ -0,0 +1,7 @@
+//Array part 2 
+
+const myArray = [1, 2, 3, 4, 5];
+
+console.log(myArray); // Output: [1, 2, 3, 4, 5]
+console.log(typeof myArray); // Output: object (arrays are a type of object in JavaScript)
+
