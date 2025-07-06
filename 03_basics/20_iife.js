5d62ef4181ada89c9889662aa343926987a10730
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-07-06 19:37:26 +0530

    IIFE

---
+++ 03_basics/20_iife.js
@@ -0,0 +1,20 @@
+//Immediately Invoked Function Expressions (IIFE)
+
+
+function chai() {
+    console.log('DB connected');
+}
+
+// chai();
+
+(function chai() {
+    console.log('TARUN BHATIA');
+})();
+
+//global scope ke pollution ko hatane ke liye hi immediate fe ka use karte hai 
+
+( (name) => {
+    console.log(`connected ${name}`);
+} ) ("Tarun")
+
+
