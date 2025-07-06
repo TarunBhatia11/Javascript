2e858e1f6f7d420cc5f7075f7ad7873525fd716a
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-07-06 19:03:32 +0530

    scopes2

---
+++ 03_basics/18_scopes2.js
@@ -0,0 +1,44 @@
+function one(){
+    const username = "tarun";
+
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two();
+}
+
+// one();
+
+
+
+if(true){
+    const username = "Tarun Bhatia";
+
+    if(username === "Tarun Bhatia"){
+        const website = " youtube";
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+console.log(addOne(5));
+function addOne(value){
+    return value + 1;
+}
+
+
+
+
+const addTwo = function (nums) {
+    return nums+ 2;
+}
+
+addTwo(5);
+
