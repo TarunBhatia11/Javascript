bd3d2f10501cd2d771772455038d4af9ebeefb50
TarunBhatia11 <tarinbhatia2809@gmail.com>
2025-07-08 16:09:29 +0530

    rqwerty

---
+++ 04_basics/script.js
@@ -0,0 +1,24 @@
+console.log("Hello world from style.js");
+
+
+
+
+
+// let button = document.getElementById("btn");
+// button.addEventListener("click", myButtonClick)
+// function myButtonClick(){
+//     console.log("Button was clicked");
+//     let para = document.createElement("p");
+//     para.innerText = "This is a new paragraph created by JS";
+// }
+
+
+let header = document.getElementById("jeader")
+console.dir(header);
+
+// console.log(header.innerText);
+
+let div = document.getElementById("div");
+console.log(div.innerHTML);
+
+
